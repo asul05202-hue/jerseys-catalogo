21c556c857570f5e3a995cdd80f78aa8901a9a36
asul05202-hue <asul05202@gmail.com>
2026-07-28 13:04:30 -0600

    add: catalog_links.js con todos los links

---
+++ catalog_links.js
@@ -0,0 +1,157 @@
+window.CATALOG_LINKS = [
+  {
+    category: "Liga MX",
+    links: [
+      { name: "Liga MX - Camisetas", url: "https://a759499776.x.yupoo.com/categories/4364554" },
+      { name: "Liga MX - Camisetas 2", url: "https://tcc918-q.x.yupoo.com/categories/4879861" },
+      { name: "Liga MX - Jugador (America, Chivas...)", url: "https://baocheng3f888.x.yupoo.com/albums/246292583?uid=1&isSubCate=false&referrercate=3517272" },
+      { name: "Liga MX - Jugador 2", url: "https://baocheng3f888.x.yupoo.com/albums/246530105?uid=1&isSubCate=false&referrercate=3517272" },
+      { name: "Liga MX - Jugador 3", url: "https://baocheng3f888.x.yupoo.com/albums/245410908?uid=1&isSubCate=false&referrercate=3517272" },
+      { name: "Liga MX - Conjunto mexicano", url: "https://hrtz522.x.yupoo.com/albums" },
+      { name: "Liga MX - Con contrasena", url: "https://huihui234.x.yupoo.com/", note: "huihui888" },
+      { name: "Liga MX - Ropa femenina", url: "https://a759499776.x.yupoo.com/categories/4453831" }
+    ]
+  },
+  {
+    category: "Selecciones Nacionales",
+    links: [
+      { name: "Selecciones y Clubes - zzs04", url: "https://zzs04.x.yupoo.com/" },
+      { name: "Selecciones - Coleccion 1", url: "https://zzs04.x.yupoo.com/collections/4659161" },
+      { name: "Selecciones - Coleccion 2", url: "https://zzs04.x.yupoo.com/collections/4792009" },
+      { name: "Selecciones y Clubes - 420jersey", url: "https://420jersy.x.yupoo.com/" },
+      { name: "Seleccion nacional y liga", url: "https://guangzhe009.x.yupoo.com/categories" },
+      { name: "Ligas y Selecciones", url: "https://18369567776.x.yupoo.com/" },
+      { name: "Version jugador y fan", url: "https://hongpintiyu.x.yupoo.com/" },
+      { name: "Version fan", url: "https://guanshen1013998228.x.yupoo.com/albums/?page=1" },
+      { name: "Version fan 2", url: "https://svip-1688.x.yupoo.com/" },
+      { name: "Version fan 3", url: "https://leshang167.x.yupoo.com/" },
+      { name: "Version fan 4", url: "https://xianghong88.x.yupoo.com/" },
+      { name: "Version jugador", url: "https://baocheng3f888.x.yupoo.com/collections/3517272" },
+      { name: "Version jugador 2", url: "https://3409834285.x.yupoo.com/" },
+      { name: "Version jugador 3", url: "https://bufan188.x.yupoo.com/albums" },
+      { name: "Jugador - Pantalones cortos", url: "https://baocheng3f888.x.yupoo.com/collections/5146701" },
+      { name: "Jugador deportivo anime japones", url: "https://18369567776.x.yupoo.com/collections/4873451" },
+      { name: "Jugador - liang", url: "https://liang19802013380.x.yupoo.com/categories/5139558" },
+      { name: "Estilo femenino", url: "https://bomandi.x.yupoo.com/categories/4782417" }
+    ]
+  },
+  {
+    category: "Europa - Clubes",
+    links: [
+      { name: "Europa Clubes - Version jugador", url: "https://baocheng3f888.x.yupoo.com/collections/3517272?page=1" },
+      { name: "Europa Clubes - zzs04", url: "https://zzs04.x.yupoo.com/collections/4659163" }
+    ]
+  },
+  {
+    category: "Retro Europa",
+    links: [
+      { name: "Retro Europa - Series", url: "https://3072503479.x.yupoo.com/categories/4685054" },
+      { name: "Retro Europa - Todo", url: "https://3072503479.x.yupoo.com/" },
+      { name: "Retro - ingsports", url: "https://ingsports0828.x.yupoo.com/" },
+      { name: "Retro - 3484linga", url: "https://3484linga.x.yupoo.com/albums" },
+      { name: "Retro - mx187", url: "https://mx18776500995mx.x.yupoo.com/albums/94816568?uid=1" }
+    ]
+  },
+  {
+    category: "Retro Liga MX",
+    links: [
+      { name: "Retro Liga MX - yuebo", url: "https://yuebojersey.x.yupoo.com/collections/4169658" },
+      { name: "Retro Mexicana - 2707", url: "https://2707499085.x.yupoo.com/" }
+    ]
+  },
+  {
+    category: "Retros - Barcelona",
+    links: [
+      { name: "Retros Barcelona - Coleccion 1", url: "https://3484linga.x.yupoo.com/collections/5137840" },
+      { name: "Retros Barcelona - Coleccion 2", url: "https://3484linga.x.yupoo.com/collections/5137842" },
+      { name: "Retros Barcelona - Coleccion 3", url: "https://3484linga.x.yupoo.com/collections/5137843" },
+      { name: "Retros Barcelona - Coleccion 4", url: "https://3484linga.x.yupoo.com/collections/5137856" },
+      { name: "Retros Barcelona - Coleccion 5", url: "https://3484linga.x.yupoo.com/collections/5137858" },
+      { name: "Retros Barcelona - Coleccion 6", url: "https://3484linga.x.yupoo.com/collections/5137857" },
+      { name: "Retros Barcelona - Coleccion 7", url: "https://3484linga.x.yupoo.com/collections/5137860" },
+      { name: "Retros Barcelona - Coleccion 8", url: "https://3484linga.x.yupoo.com/collections/5137859" },
+      { name: "Retros Barcelona - Album 1", url: "https://3484linga.x.yupoo.com/albums/226181458?uid=1&isSubCate=false&referrercate=5138186" },
+      { name: "Retros Barcelona - Album 2", url: "https://3484linga.x.yupoo.com/albums/245978523?uid=1&isSubCate=false&referrercate=5138209" },
+      { name: "Retros Barcelona - 5138176", url: "https://3484linga.x.yupoo.com/collections/5138176" },
+      { name: "Retros Barcelona - 5138197", url: "https://3484linga.x.yupoo.com/collections/5138197" },
+      { name: "Retros Barcelona - 5143104", url: "https://3484linga.x.yupoo.com/collections/5143104" },
+      { name: "Retros Barcelona - 5234447", url: "https://3484linga.x.yupoo.com/collections/5234447" },
+      { name: "Retros Barcelona - 5146191", url: "https://3484linga.x.yupoo.com/collections/5146191" },
+      { name: "Retros Barcelona - 5138203", url: "https://3484linga.x.yupoo.com/collections/5138203" },
+      { name: "Retros Barcelona - 5138206", url: "https://3484linga.x.yupoo.com/collections/5138206" },
+      { name: "Retros Barcelona - 5138210", url: "https://3484linga.x.yupoo.com/collections/5138210" },
+      { name: "Retros Barcelona - 5138211", url: "https://3484linga.x.yupoo.com/collections/5138211" },
+      { name: "Retros Barcelona - 5138208", url: "https://3484linga.x.yupoo.com/collections/5138208" },
+      { name: "Retros Barcelona - 5138204", url: "https://3484linga.x.yupoo.com/collections/5138204" },
+      { name: "Retros Barcelona - 5138207", url: "https://3484linga.x.yupoo.com/collections/5138207" },
+      { name: "Retros Barcelona - 5138200", url: "https://3484linga.x.yupoo.com/collections/5138200" },
+      { name: "Retros Barcelona - 5138199", url: "https://3484linga.x.yupoo.com/collections/5138199" },
+      { name: "Retros Barcelona - 5138198", url: "https://3484linga.x.yupoo.com/collections/5138198" },
+      { name: "Retros Barcelona - 5138195", url: "https://3484linga.x.yupoo.com/collections/5138195" },
+      { name: "Retros Barcelona - 5138189", url: "https://3484linga.x.yupoo.com/collections/5138189" },
+      { name: "Retros Barcelona - 5138194", url: "https://3484linga.x.yupoo.com/collections/5138194" },
+      { name: "Retros Barcelona - 5138191", url: "https://3484linga.x.yupoo.com/collections/5138191" },
+      { name: "Retros Barcelona - 5138190", url: "https://3484linga.x.yupoo.com/collections/5138190" },
+      { name: "Retros Barcelona - 5138184", url: "https://3484linga.x.yupoo.com/collections/5138184" },
+      { name: "Retros Barcelona - 5138183", url: "https://3484linga.x.yupoo.com/collections/5138183" },
+      { name: "Retros Barcelona - 5138181", url: "https://3484linga.x.yupoo.com/collections/5138181" },
+      { name: "Retros Barcelona - 5138188", url: "https://3484linga.x.yupoo.com/collections/5138188" },
+      { name: "Retros Barcelona - 5138175", url: "https://3484linga.x.yupoo.com/collections/5138175" },
+      { name: "Retros Barcelona - 5138173", url: "https://3484linga.x.yupoo.com/collections/5138173" }
+    ]
+  },
+  {
+    category: "NBA / NHL / NFL / MLB",
+    links: [
+      { name: "NBA, NHL, NFL, MLB - Completo", url: "https://linrirong1314.x.yupoo.com/categories", note: "778899" },
+      { name: "Uniformes de baloncesto", url: "https://qiuqi-sports.x.yupoo.com/" },
+      { name: "Uniformes de baloncesto 2", url: "https://lan-xing.x.yupoo.com/" },
+      { name: "Uniformes de baloncesto 3", url: "https://xingkong-sports.x.yupoo.com/" },
+      { name: "Ropa NBA", url: "https://94nbaqiuyi.x.yupoo.com/" },
+      { name: "Pantalones y ropa", url: "https://cz9007.x.yupoo.com/" },
+      { name: "NFL - Futbol americano", url: "https://nfl-cyq888.x.zhidian-inc.cn/" },
+      { name: "Shorts - Con contrasena", url: "https://xingda88.x.yupoo.com/", note: "667788" },
+      { name: "NBA Termosellado", url: "https://pifajersey.x.yupoo.com/albums" }
+    ]
+  },
+  {
+    category: "Uniformes de Entrenamiento",
+    links: [
+      { name: "Conjunto entrenamiento - changjiangsports", url: "https://x.yupoo.com/photos/changjiangsports/albums?tab=gallery" },
+      { name: "Traje entrenamiento - huang456852", url: "https://huang456852.x.yupoo.com/albums" },
+      { name: "Traje entrenamiento - aosendi", url: "https://aosendi.x.yupoo.com/albums" },
+      { name: "Conjuntos entrenamiento - dongshanstore", url: "https://dongshanstore.x.yupoo.com/albums" },
+      { name: "Uniforme entrenamiento - 007007", url: "https://007007haoyuntiyu.x.yupoo.com/" },
+      { name: "Traje entrenamiento - kuanghanyundong", url: "https://kuanghanyundong.x.yupoo.com/albums" }
+    ]
+  },
+  {
+    category: "Playeras de Beisbol",
+    links: [
+      { name: "Beisbol - zzs04", url: "https://zzs04.x.yupoo.com/collections/4970751" }
+    ]
+  },
+  {
+    category: "Sachets / Jackets",
+    links: [
+      { name: "Jackets - zzs04", url: "https://zzs04.x.yupoo.com/collections/4911006" },
+      { name: "Chaqueta cortavientos - 1215795243", url: "https://1215795243.x.yupoo.com/categories" },
+      { name: "Chaqueta cortavientos - ty-guoji", url: "https://ty-guoji.x.yupoo.com/albums" }
+    ]
+  },
+  {
+    category: "Shorts",
+    links: [
+      { name: "Shorts - zzs04", url: "https://zzs04.x.yupoo.com/collections/4834911" }
+    ]
+  },
+  {
+    category: "Ropa Informal / Casual",
+    links: [
+      { name: "Ropa casual - lisa1059", url: "https://lisa1059.x.yupoo.com" },
+      { name: "Ropa casual - ting8899", url: "https://ting8899.x.yupoo.com/" },
+      { name: "Ropa casual - ty-guoji", url: "https://ty-guoji.x.yupoo.com/categories" },
+      { name: "F1", url: "https://yiyisports2016.x.yupoo.com/" }
+    ]
+  }
+];
