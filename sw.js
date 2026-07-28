439913fb56ac7784c23c38426bf63d6ae98d57d6
asul05202-hue <asul05202@gmail.com>
2026-07-27 21:55:58 -0600

    add: sw desactivado

---
+++ sw.js
@@ -0,0 +1,5 @@
+// SW DESACTIVADO
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
